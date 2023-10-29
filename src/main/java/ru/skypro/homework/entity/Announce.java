9865f6eaec7bcd7060383c4c9c6436eaff2c0e52
HardR1se <stepan_morozov_1980@mail.ru>
2023-10-29 23:11:32 +0700

    Добавил описание в контроллер + мапер + entity + набросал немного реализацию методов в сервисе

--- src/main/java/ru/skypro/homework/entity/Announce.java
+++ src/main/java/ru/skypro/homework/entity/Announce.java
@@ -1,22 +1,32 @@
-package ru.skypro.homework.dto.announce;
+package ru.skypro.homework.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Announce {
-    private int pk;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long pk;
     private String authorFirstName;
     private String authorLastName;
     private String description;
     private String email;
     private String image;
     private String phone;
-    private int price;
+    private Long price;
     private String title;
 }
--- src/main/java/ru/skypro/homework/service/AnnounceService.java
+++ src/main/java/ru/skypro/homework/service/AnnounceService.java
@@ -1,18 +1,19 @@
 package ru.skypro.homework.service;
 
 import org.springframework.web.multipart.MultipartFile;
-import ru.skypro.homework.dto.announce.AnnounceOut;
-import ru.skypro.homework.dto.announce.Announce;
 import ru.skypro.homework.dto.announce.CreateOrUpdateAd;
+import ru.skypro.homework.entity.Announce;
+import ru.skypro.homework.dto.announce.AnnounceDtoOut;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface AnnounceService {
-    List<AnnounceOut> getUsersAll();
-    List<AnnounceOut> getAll();
+    List<AnnounceDtoOut> getUsersAll();
+    List<AnnounceDtoOut> getAll();
     Announce get(Long id);
-    AnnounceOut add(CreateOrUpdateAd properties, MultipartFile image);
-    AnnounceOut updateInfo(Long id, CreateOrUpdateAd property);
-    void updateImage(Long id, MultipartFile image);
+    AnnounceDtoOut add(CreateOrUpdateAd properties, MultipartFile image) throws IOException;
+    AnnounceDtoOut updateInfo(Long id, CreateOrUpdateAd property);
+    void updateImage(Long id, MultipartFile image) throws IOException;
     void delete(Long id);
 }
