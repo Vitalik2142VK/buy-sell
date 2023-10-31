1aa7f7c071c469c6391743b84d10de7066dfcb04
HardR1se <stepan_morozov_1980@mail.ru>
2023-10-31 22:42:49 +0700

    Корректировки

--- src/main/java/ru/skypro/homework/entity/Announce.java
+++ src/main/java/ru/skypro/homework/entity/Announce.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.stereotype.Repository;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -20,13 +19,13 @@ public class Announce {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long pk;
+    private int pk;
     private String authorFirstName;
     private String authorLastName;
     private String description;
     private String email;
     private String image;
     private String phone;
-    private Long price;
+    private int price;
     private String title;
 }
